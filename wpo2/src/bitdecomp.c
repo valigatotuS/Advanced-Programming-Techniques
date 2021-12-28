e7ff075ea3275c5b302a6c1dee9c4661ee8090bf
valigatotuS <valentin.quevy@gmail.com>
2021-12-28 18:37:28 +0100

    bitdecomp update

--- wpo2/src/bitdecomp.c
+++ wpo2/src/bitdecomp.c
@@ -2,15 +2,15 @@
     By: @vq
     Created: 2021/12/16 by vq
 */
-
 #include "wpo2_lib.h"
 
+#define PRECISION 16
 #define getbit(num, pos) (num>>pos&1)
 
 void BitDecomp(){
     int num;
     scanf("%i", &num);
-    for (int i = 4; i >= 0; i--){
+    for (int i = PRECISION-1; i >= 0; i--){
         printf("%i", getbit(num, i));
         if(i%4==0) printf(" | ");
     }
