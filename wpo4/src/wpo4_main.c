8f5dda4be1753b227fe08e2f4992266ef961a976
valigatotuS <valentin.quevy@gmail.com>
2021-12-28 18:57:14 +0100

    create wpo4

---
+++ wpo4/src/wpo4_main.c
@@ -0,0 +1,18 @@
+#include "wpo4_lib.h"
+
+int main() {
+    /*
+        Comment/Uncomment desired code to test the functions 
+    */
+
+    #pragma region intro code
+
+    "...";
+
+    #pragma endregion
+
+    #pragma region exercise1
+    #pragma endregion
+
+    return 0;
+}
